41aded0b6e942df77f93d406a1cad100752a03e7
Christian Kitte <christian.kitte@stud.hs-emden-leer.de>
2021-03-01 16:38:44 +0100

    Projektdateien hinzufügen.

---
+++ RestClient/Controllers/PersonsController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using RestClient.Models;
+
+namespace RestClient.Controllers
+{
+    public class PersonsController : Controller
+    {
+        // GET: PersonsController
+        public ActionResult Index()
+        {
+            List<Person> persons = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://127.0.0.1:8080/api/v1/");
+                persons = client.GetFromJsonAsync<List<Person>>("persons/").Result;
+            }
+
+            return View(persons);
+        }
+
+        // GET: PersonsController/Details/5
+        public ActionResult Details(int id)
+        {
+            Person person = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://127.0.0.1:8080/api/v1/");
+                person = client.GetFromJsonAsync<Person>("persons/" + id.ToString() + "/").Result;
+            }
+
+            return View(person);
+        }
+
+        // GET: PersonsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://127.0.0.1:8080/api/v1/");
+                    var x = "persons/" + collection["Vorname"] + "/" + collection["Nachname"] + "/";
+                    var person =
+                        client.PostAsync("persons/" + collection["Vorname"] + "/" + collection["Nachname"] + "/", null);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RestClient/Models/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestClient.Models
+{
+    public class Person
+    {
+        /// <summary>
+        /// Die ID der Person 
+        /// </summary>
+        [Key]
+        public int PersonID { get; set; }
+
+        /// <summary>
+        /// Der Vorname der Person                  
+        /// </summary>
+        [Required]
+        public string Vorname { get; set; }
+
+        /// <summary>
+        /// Der Nachname der Person 
+        /// </summary>
+        [Required]
+        public string Nachname { get; set; }
+
+        /// <summary>
+        /// Überschreibt die ToString() Methode
+        /// </summary>
+        /// <returns>Einen String, der die Entität beschreibt</returns>
+        public override string ToString()
+        {
+            return String.Format("Person mit der ID {0} und dem Namen {1} {2} wurde geholt",
+                new String[] {this.PersonID.ToString(), this.Vorname, this.Nachname});
+        }
+    }
+}
